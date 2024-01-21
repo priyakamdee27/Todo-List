69f71fee9ddca81e9cfac97934fbd3b6f3978d03
Priya <shikhakamdee@gmail.com>
2024-01-21 08:35:27 +0530

    initial commit

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.add_item, name='add_item'),
-    path('<int:id>/', views.add_item, name='update_item'),
-    path('delete/<int:id>', views.delete_item, name='delete_item'),
-    path('list/', views.item_list, name='item_list'),
+    path("add", views.add_item, name="add_item"),
+    path("<int:id>/", views.add_item, name="update_item"),
+    path("delete/<int:id>", views.delete_item, name="delete_item"),
+    path("", views.item_list, name="item_list"),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render, redirect
 from .forms import TodoForm
 from .models import TodoApp
+
 # Create your views here.
 
 
 def add_item(request, id=0):
-    if request.method == 'GET':
+    if request.method == "GET":
         if id == 0:
             form = TodoForm()
         else:
             todo = TodoApp.objects.get(pk=id)
             form = TodoForm(instance=todo)
 
-        return render(request, 'add_item.html', {'form': form})
+        return render(request, "add_item.html", {"form": form})
     else:
         if id == 0:
             form = TodoForm(request.POST)
@@ -21,15 +22,15 @@ def add_item(request, id=0):
             form = TodoForm(request.POST, instance=todo)
     if form.is_valid():
         form.save()
-    return redirect('/todo/list')
+    return redirect("/")
 
 
 def item_list(request):
-    context = {'item_list': TodoApp.objects.all()}
+    context = {"item_list": TodoApp.objects.all()}
     return render(request, "item_list.html", context)
 
 
 def delete_item(request, id):
     todo = TodoApp.objects.get(pk=id)
     todo.delete()
-    return redirect('/todo/list')
+    return redirect("/")
